3f68ec90188b7f1ab98465429776ac8eda131b6a
Mohammad Ali Karimi <ali216k@gmail.com>
2023-02-22 21:07:26 +0000

    random number has  done!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,10 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/square_results.html.erb" })
   end
 
+  def blank_random_form
+    render({ :template => "calculation_templates/random_form.html.erb" })
+  end
+
   def calculate_random
 
     @lower = params.fetch("user_min").to_f
