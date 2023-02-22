58f165673cf1162e34b55060d5e7ea6223cc80a7
Mohammad Ali Karimi <ali216k@gmail.com>
2023-02-22 20:45:10 +0000

    Get random number

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,13 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/square_results.html.erb" })
   end
 
+  def calculate_random
+
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
+
+    render({ :template => "calculation_templates/rand_results.html.erb" })
+  end
+
 end
